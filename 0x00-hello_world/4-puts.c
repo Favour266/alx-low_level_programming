4b5f6e778e94041bd39582915ac7af3d9b5ff8d8
Okeke <innocentfavour610@gmail.com>
2022-03-10 23:41:30 -0800

    cool

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Desscription: main - prints out a string to stdout.
- * Return: 0 if success.
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
