c3cd68456eaab6166bae610ba8eef956aa560881
Okeke <innocentfavour610@gmail.com>
2022-03-11 15:42:19 -0800

    print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,23 +2,23 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
-{	
-int i = 0;
-
-while (i < 10)
 {
-putchar(i + '0');
-if (i < 9)
-{
-putchar(44);
-putchar(32);
-}
-i++;
-}
-putchar('\n');
-return (0);
-}
+	int i = 0;
 
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
