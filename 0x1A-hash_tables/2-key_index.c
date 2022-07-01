2d4bb25018fa1a833bab9cf1dbc1ddbf0528f28a
Okeke <innocentfavour610@gmail.com>
2022-07-01 07:02:14 -0700

    index key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,21 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - function that generates a random number through
- * a string given (hash number)
- *
- * @str: string
- * Return: hash number
+ * key_index - generates the key using hash_djb2
+ * @key: pointer the key
+ * @size: size table
+ * Return: index key
  */
-unsigned long int hash_djb2(const unsigned char *str)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
+	return (hash_djb2(key) % size);
 }
